3f3b9ccce360e06a8426c0e936eafef05c0437fd
Astrid <astrid@maigrueen.de>
2018-07-01 18:48:30 +0200

    Chart shown, Object not working yet

--- FTEchart.js
+++ FTEchart.js
@@ -10,17 +10,48 @@ submit = document.querySelector("#btn"),
 FTEinput = [];
 
 var buildObject = function() {
-  //FTEinput.push({valueFEa: FEavailable.value}); ??
-  console.log(FEavailable.value);
-  //alle Inputs in ein Objekt schreiben
-};
+  FTEinput.push({valueFEa: FEavailable.value, valueFEr: FErequired.value, valueBEa: BEavailable.value, valueBEr: BErequired.value,
+    valueQAa: QAavailable.value, valueQAr: QArequired.value, valueTPMa: TPMavailable.value, valueTPMr: TPMrequired.value});
+    console.log(FTEinput.valueFEa);
+    buildChart();
+  };
+  submit.addEventListener("click", buildObject);
 
-submit.addEventListener("click", buildObject);
+  var calculateFTE = function() {
+    //für jede Säule ausrechnen, welcher Wert größer ist
+  };
 
-var calculateFTE = function() {
-  //für jede Säule ausrechnen, welcher Wert größer ist
-};
-
-var buildChart = function() {
-  //irgendwie mit Chartjs etwas bauen
-};
+  var buildChart = function() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["FE", "BE", "QA", "TPM"],
+        datasets: [{
+          label: 'Available',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          data: [
+            FTEinput.valueFEa,
+            FTEinput.valueBEa,
+            FTEinput.valueQAa,
+            FTEinput.valueTPMa]
+        }, {
+          label: 'Required',
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          data: [
+            FTEinput.valueFEr,
+            FTEinput.valueBEr,
+            FTEinput.valueQAr,
+            FTEinput.valueTPMr]
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            stacked: true
+          }]
+        }
+      }
+    });
+    console.log(FTEinput.valueFEa);
+  };
